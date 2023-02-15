dcaa0e4169d09f2777677119f679744d1dcb1110
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-15 16:09:35 +0800

    refactor renamed platform folder to network

--- lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
+++ lib/features/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -1,14 +1,13 @@
-import 'package:clean_architecture_tdd_resocoder/features/number_trivia/data/models/number_trivia_model.dart';
-
-import '../../../../core/error/exceptions.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../../../core/error/exceptions.dart';
 import '../../../../core/error/failures.dart';
-import '../../../../core/platform/network_info.dart';
+import '../../../../core/network/network_info.dart';
 import '../../domain/entities/number_trivia.dart';
 import '../../domain/repositories/number_trivia_repository.dart';
 import '../datasources/number_trivia_local_data_source.dart';
 import '../datasources/number_trivia_remote_data_source.dart';
+import '../models/number_trivia_model.dart';
 
 typedef _ConcreteOrRandomChooser = Future<NumberTriviaModel> Function();
 
--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,6 +1,6 @@
 import 'package:clean_architecture_tdd_resocoder/core/error/exceptions.dart';
 import 'package:clean_architecture_tdd_resocoder/core/error/failures.dart';
-import 'package:clean_architecture_tdd_resocoder/core/platform/network_info.dart';
+import 'package:clean_architecture_tdd_resocoder/core/network/network_info.dart';
 import 'package:clean_architecture_tdd_resocoder/features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
 import 'package:clean_architecture_tdd_resocoder/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
 import 'package:clean_architecture_tdd_resocoder/features/number_trivia/data/models/number_trivia_model.dart';

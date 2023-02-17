1ac22900520da9b25b3e0a5d4f51f054430bb794
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-17 17:04:38 +0800

    add error handling for getting trivia for concrete

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -6,6 +6,7 @@ import 'package:equatable/equatable.dart';
 import 'package:clean_architecture_tdd_resocoder/core/util/input_converter.dart';
 import 'package:clean_architecture_tdd_resocoder/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
+import '../../../../core/error/failures.dart';
 import '../../domain/entities/number_trivia.dart';
 import '../../domain/usecases/get_concrete_number_trivia.dart';
 
@@ -37,9 +38,26 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
           emit(const Error(message: INVALID_INPUT_FAILURE_MESSAGE));
         },
         (integer) async {
-          getConcreteNumberTrivia(Params(number: integer));
+          emit(Loading());
+          final failureOrTrivia =
+              await getConcreteNumberTrivia(Params(number: integer));
+          emit(failureOrTrivia.fold(
+            (failure) => Error(message: _mapFailureToMessage(failure)),
+            (trivia) => Loaded(trivia: trivia),
+          ));
         },
       );
     });
+  }
+
+  String _mapFailureToMessage(failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected Error';
+    }
   }
 }

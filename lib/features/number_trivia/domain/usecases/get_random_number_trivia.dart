220eb431eaa5a84a4ab00bcfd30ff925c3a3a749
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-19 17:04:57 +0800

    fix remove nullable

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -5,13 +5,13 @@ import '../../../../core/usecases/usecase.dart';
 import '../entities/number_trivia.dart';
 import '../repositories/number_trivia_repository.dart';
 
-class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams?> {
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
   final NumberTriviaRepository repository;
 
   GetRandomNumberTrivia(this.repository);
 
   @override
-  Future<Either<Failure, NumberTrivia>>? call(NoParams? params) async {
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
     return await repository.getRandomNumberTrivia();
   }
 }

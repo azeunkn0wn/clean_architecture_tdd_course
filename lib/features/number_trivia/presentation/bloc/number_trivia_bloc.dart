36ed9f593de30492a175d9d58cfc8b82ed8c0177
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-21 15:24:55 +0800

    refactor imports

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,10 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
 import 'package:clean_architecture_tdd_resocoder/core/util/input_converter.dart';
 import 'package:clean_architecture_tdd_resocoder/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:equatable/equatable.dart';
 
 import '../../../../core/error/failures.dart';
 import '../../../../core/usecases/usecase.dart';

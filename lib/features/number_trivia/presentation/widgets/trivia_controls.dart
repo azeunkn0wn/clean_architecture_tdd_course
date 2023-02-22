01fd5cb132fa1412613ae0f9a68f6940f5ef0d15
azeunkn0wn <azeunkn0wn@gmail.com>
2023-02-22 20:36:36 +0800

    add textfield, and buttons number trivia search

---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/number_trivia_bloc.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<TriviaControls> createState() => _TriviaControlsState();
+}
+
+class _TriviaControlsState extends State<TriviaControls> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TextField(
+          controller: _controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a Number',
+          ),
+          // onChanged: ((value) {
+          //   inputStr = value;
+
+          // }),
+          onSubmitted: (_) {
+            dispatchConcrete();
+          },
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchConcrete,
+                child: const Text('Search'),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchRandom,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).colorScheme.secondary,
+                ),
+                child: Text(
+                  'Get random trivia',
+                  style:
+                      TextStyle(color: Theme.of(context).colorScheme.primary),
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    BlocProvider.of<NumberTriviaBloc>(context)
+        .add(GetTriviaForConcreteNumber(_controller.text));
+    _controller.clear();
+  }
+
+  void dispatchRandom() {
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForRandomNumber());
+    _controller.clear();
+  }
+}
